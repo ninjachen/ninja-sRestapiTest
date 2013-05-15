06a63a3e0e9fa7cfcc7957aad4fb71d53b2e30d9
ninja <jiachenning@gmail.com>
2013-05-15 23:42:23 +0800

    new button

--- RestApiTest/RestApiTest/ViewController.m
+++ RestApiTest/RestApiTest/ViewController.m
@@ -18,13 +18,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    //1
+    //CGRect frameForWindow = [[UIScreen mainScreen] bounds];
     
-    CGRect frameForWindow = [[UIScreen mainScreen] bounds];
-    
-    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(50,50,frameForWindow.size.width,frameForWindow.size.height) ];
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(50,50,50,50) ];
+    UIButton *button1 = [[UIButton alloc] initWithFrame:CGRectMake(50,50,50,50) ];
     //UITextField *text = [[UIButton alloc] initWithFrame:CGRectMake(10,10,frameForWindow.size.width,frameForWindow.size.height) ];
     self.workerIdTextField = [[UITextField alloc] initWithFrame:CGRectMake(150, 14, 160, 30)] ;
-    
+    self.workerIdTextField.text= @"初始化字符串";
+    [button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [button setBackgroundImage: [UIImage imageNamed:@"settingButton.png"] forState:UIControlStateNormal];
+    //[self.view addSubview:button];
+    //[self.view addSubview:_workerIdTextField];
     
 }
 
@@ -34,4 +39,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Event Method
+
+- (IBAction)buttonPressed:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
 @end
